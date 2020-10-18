5ec748e26a7c0966b92c9e91e2af3c4d7f30b75e
Isaiah Ewing <50162411+Shaiai@users.noreply.github.com>
2020-10-17 20:10:30 -0500

    RoomToRoom / Hurtboxes / Ai behavior
    - Added the name cards for room transitions.
    - Added hurt / hit boxes to sample enemy and main character.
    - Handled collision and knock back physics for both as well.
    - Health is set but damage is not.
    - Made optimal for all enemies / Characters / Objects.

---
+++ Blood Rites/Assets/Scripts/RoomMove.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomMove : MonoBehaviour
+{
+
+    public bool needText;
+    public string placeName;
+    public GameObject text;
+    public Text placeText;
+
+
+    //Reference to my CameraMovement Script
+    private CameraMovement cam;
+
+    //Warps camera from trigger point to a point in the next room..
+    public Vector2 cameraChange;
+    //Warps place of player from the trigger point to the other room.
+    public Vector3 playerChange;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.GetComponent<CameraMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            cam.minPosition += cameraChange;
+            cam.maxPosition += cameraChange;
+            other.transform.position += playerChange;
+            
+            if(needText)
+            {
+              StartCoroutine(placeNameCo());
+            }
+        }
+    }
+
+    private IEnumerator placeNameCo()
+    {
+        text.SetActive(true);
+        placeText.text = placeName;
+        yield return new WaitForSeconds(4f);
+        text.SetActive(false);
+    }
+}
