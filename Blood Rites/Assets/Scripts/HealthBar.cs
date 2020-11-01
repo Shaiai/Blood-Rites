da838f4cc8e4a03e6eb27109aae2886496143c15
Isaiah Ewing <50162411+Shaiai@users.noreply.github.com>
2020-11-01 17:07:27 -0600

    Fixed Health Bar
    - Healthbar now reflects players properly and changes gradient based on health percentage.

--- Blood Rites/Assets/Scripts/HealthBar.cs
+++ Blood Rites/Assets/Scripts/HealthBar.cs
@@ -6,18 +6,21 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public Slider slider;
+    public Gradient gradient;
+    public Image fill;
     // Start is called before the first frame update
    
     public void SetHealth(int health)
     {
-        slider.maxValue = health;
         slider.value = health;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 
     public void SetMaxHealth(int health)
     {
-       
+        slider.maxValue = health;
         slider.value = health;
+        fill.color = gradient.Evaluate(1f);
     }
 
 }
