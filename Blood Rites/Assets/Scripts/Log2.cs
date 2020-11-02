a7ff1618c85093ff6c9dbd0c7e1f631bccc34ed9
Isaiah Ewing <50162411+Shaiai@users.noreply.github.com>
2020-11-01 23:38:33 -0600

    new enemy movement and chase system.

---
+++ Blood Rites/Assets/Scripts/Log2.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Log2 : MonoBehaviour
+{
+
+    private Animator anim;
+    private Transform target;
+    public Transform homePos;
+    [SerializeField]
+    private float speed = 0;
+    [SerializeField]
+    private float maxRange = 0;
+    [SerializeField]
+    private float minRange = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        target = FindObjectOfType<PlayerMovement>().transform;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(target.position, transform.position) <= maxRange && Vector3.Distance(target.position, transform.position) >= minRange)
+        {
+        anim.SetBool("wakeUp", true);
+        FollowPlayer();
+        }
+        else if(Vector3.Distance(target.position, transform.position) >= maxRange)
+        {
+            ReturnHome();
+        }
+    }
+
+    public void FollowPlayer()
+    {
+        anim.SetFloat("moveX", (target.position.x - transform.position.x));
+        anim.SetFloat("moveY", (target.position.y - transform.position.y));
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);  
+    }
+
+    public void ReturnHome()
+    {
+        anim.SetFloat("moveX", (homePos.position.x - transform.position.x));
+        anim.SetFloat("moveY", (homePos.position.y - transform.position.y));
+        transform.position = Vector3.MoveTowards(transform.position, homePos.position, speed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, homePos.position) == 0)
+        {
+            anim.SetBool("wakeUp",false);
+            anim.SetBool("SleepNow",true);
+        }
+    }
+
+  
+}
---
+++ Blood Rites/Assets/Scripts/PlayerMovement2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2 : MonoBehaviour
+{
+    private Rigidbody2D myPlayer;
+    private Animator anim;
+
+
+
+    [SerializeField]
+    private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+       myPlayer = GetComponent<Rigidbody2D>(); 
+       anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       myPlayer.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * (speed * Time.deltaTime);
+
+        if(myPlayer.velocity != Vector2.zero)
+        {
+        anim.SetFloat("moveX", myPlayer.velocity.x);
+        anim.SetFloat("moveY", myPlayer.velocity.y);
+        anim.SetBool("moving", true);
+        }
+        else
+        {
+        anim.SetBool("moving", false);
+        }
+
+    }
+}
